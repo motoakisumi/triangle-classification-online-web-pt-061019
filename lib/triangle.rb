1d40e372a4fd8311c5d9cf5fc632b1cfac6c7c9d
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-17 18:50:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,26 +1,29 @@
 class Triangle
-  # write code here
-  attr_reader :s1, :s2, :s3
-
-  def initialize(s1, s2, s3)
-    @s1 = s1
-    @s2 = s2
-    @s3 = s3
+  attr_reader :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
   end
 
   def kind
-
-    if @s1 + @s2 < @s3 || @s1 + @s3 < @s2 || @s2 + @s3 < @s1
-      raise TriangleError
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
     else
-      if @s1 = @s2 = @s3
-        return :equilateral
-      elsif @s1 = @s2 || @s1 = @s3 || @s2 = @s3
-        return :isosceles
-      elsif @s1 != @s2 && @s1 != @s3 && @s2 != @s3
-        return :scalene
-      end
+      :scalene
     end
+  end
+
+  def validate_triangle
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each do |side|
+      real_triangle << false if side <= 0 
+    raise TriangleError if real_triangle.include?(false)
+    end
+  end
 
   class TriangleError < StandardError
   end
