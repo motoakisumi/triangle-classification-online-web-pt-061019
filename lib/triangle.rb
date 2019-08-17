6ebbb22a8ff73fffce03eebd6287f676507330b0
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-17 18:43:19 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
   # write code here
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+
+    if @s1 + @s2 < @s3 || @s1 + @s3 < @s2 || @s2 + @s3 < @s1
+      raise TriangleError
+    else
+      if @s1 = @s2 = @s3
+        return :equilateral
+
+      elsif @s1 = @s2 || @s1 = @s3 || @s2 = @s3
+        return :isosceles
+
+      elsif @s1 != @s2 && @s1 != @s3 && @s2 != @s3
+        return :scalene
+
+      end
+    end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
+
 end
