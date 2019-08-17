d07e9b04ee8dbe028800efb7baa51724491fab10
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-17 18:58:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
 
   def kind
 
-    if (@s1 + @s2) < @s3 || (@s1 + @s3) < @s2 || (@s2 + @s3) < @s1 || @s1 == 0 || @s2 ==0 || @s3 == 0
+    if (@s1 + @s2) <= @s3 || (@s1 + @s3) <= @s2 || (@s2 + @s3) <= @s1 || @s1 == 0 || @s2 ==0 || @s3 == 0
       raise TriangleError
     else
       if @s1 == @s2 && @s2== @s3
