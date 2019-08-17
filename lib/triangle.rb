b2650b6f8a57afa4aa53893148875f90becb50e1
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-17 18:59:06 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,20 +10,18 @@ class Triangle
 
   def kind
 
-    if @s1 + @s2 < @s3 || @s1 + @s3 < @s2 || @s2 + @s3 < @s1
+    if (@s1 + @s2) <= @s3 || (@s1 + @s3) <= @s2 || (@s2 + @s3) <= @s1 || @s1 == 0 || @s2 ==0 || @s3 == 0
       raise TriangleError
     else
-      if @s1 = @s2 = @s3
+      if @s1 == @s2 && @s2== @s3
         return :equilateral
-
-      elsif @s1 = @s2 || @s1 = @s3 || @s2 = @s3
+      elsif @s1 == @s2 || @s1 == @s3 || @s2 == @s3
         return :isosceles
-
       elsif @s1 != @s2 && @s1 != @s3 && @s2 != @s3
         return :scalene
-
       end
     end
+  end
 
   class TriangleError < StandardError
   end
