2afa5a7a84aad9f701a6f49a715c374a95bf8058
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-17 18:55:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,21 +3,21 @@ class Triangle
   attr_reader :s1, :s2, :s3
 
   def initialize(s1, s2, s3)
-    @s1 = s1
-    @s2 = s2
-    @s3 = s3
+    s1 = s1
+    s2 = s2
+    s3 = s3
   end
 
   def kind
 
-    if (@s1 + @s2) < @s3 || (@s1 + @s3) < @s2 || (@s2 + @s3) < @s1
+    if (s1 + s2) < s3 || (s1 + s3) < s2 || (s2 + s3) < s1
       raise TriangleError
     else
-      if @s1 == @s2 == @s3
+      if s1 == s2 == s3
         return :equilateral
-      elsif @s1 == @s2 || @s1 == @s3 || @s2 == @s3
+      elsif s1 == s2 || s1 == s3 || s2 == s3
         return :isosceles
-      elsif @s1 != @s2 && @s1 != @s3 && @s2 != @s3
+      elsif s1 != s2 && s1 != s3 && s2 != s3
         return :scalene
       end
     end
